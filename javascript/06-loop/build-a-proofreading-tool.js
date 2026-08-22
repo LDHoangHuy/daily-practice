5fbc9a96b4d03ae24d525b3a0938b98fac07410d
Le Dang Hoang Huy <ledanghoanghuy2004@gmail.com>
2026-08-22 13:40:04 +0700

    loop-lab: build a proofreading tool

--- javascript/06-loop/build-a-proofreading-tool.js
+++ javascript/06-loop/build-a-proofreading-tool.js
@@ -1,21 +1,61 @@
 function isPalindrome(word) {
-  return word === word.split("").reverse().join("")
+  const lowerCasedWord = word.toLowerCase();
+  return lowerCasedWord === lowerCasedWord.split("").reverse().join("");
 }
 
 function findPalindromeBreaks(words) {
   const result = [];
+
   for (let i = 0; i < words.length; i++) {
     if (!isPalindrome(words[i])) {
       result.push(i);
     }
   }
+
   return result;
 }
 
 function findRepeatedPhrases(words, phraseLength) {
-  
+  if (phraseLength >= words.length) {
+    return [];
+  }
+
+  const result = [];
+  const phraseIndices = {};
+
+  for (let i = 0; i + phraseLength <= words.length; i++) {
+    const phrase = words.slice(i, i + phraseLength).join(" ");
+
+    if (phraseIndices[phrase]) {
+      phraseIndices[phrase].push(i);
+    } else {
+      phraseIndices[phrase] = [i];
+    }
+  }
+
+  for (const phrase in phraseIndices) {
+    if (phraseIndices[phrase].length > 1) {
+      result.push(...phraseIndices[phrase]);
+    }
+  }
+
+  return result;
 }
 
 function analyzeTexts(texts, phraseLength) {
+  const result = [];
 
+  for (const words of texts) {
+    const repeatedPhrases = findRepeatedPhrases(words, phraseLength);
+    const palindromeBreaks = findPalindromeBreaks(words);
+
+    result.push({
+      repeatedPhrases,
+      palindromeBreaks,
+    });
+  }
+
+  return result;
 }
+
+console.log(analyzeTexts([["the", "cat", "tat", "the", "cat"], []], 2));

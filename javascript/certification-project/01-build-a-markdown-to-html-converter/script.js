b6d102104841dda261778eff178b443a52839539
Le Dang Hoang Huy <ledanghoanghuy2004@gmail.com>
2026-08-25 23:27:33 +0700

    cert-prj01: add link regex convert to html function

--- javascript/certification-project/01-build-a-markdown-to-html-converter/script.js
+++ javascript/certification-project/01-build-a-markdown-to-html-converter/script.js
@@ -6,7 +6,8 @@ const preview = document.querySelector("#preview");
 //^(?<before>.*) ... (?<after>.*)$
 const hReg = /^\s*(?<mark>#{1,3})\s(?<text>.+)/gm;
 const strEmReg = /(?<mark>\*{1,2}|_{1,2})(?<text>.*)\k<mark>/g;
-const imgReg = /!\[(?<altText>.*)\]\((?<imgSrc>.*)\)/y;
+const imgReg = /!\[(?<altText>.*)\]\((?<imgSrc>.*)\)/g;
+const linkReg = /\[(?<linkText>.*)\]\((?<url>.*)\)/g;
 
 //`<p>${groups.before}<strong>${groups.text}</strong>${groups.after}</p>`
 //`<p>${groups.before}<em>${groups.text}</em>${groups.after}</p>`
@@ -28,6 +29,12 @@ textArea.addEventListener("input", () => {
       const alt = groups.altText.trim();
       const src = groups.imgSrc.trim();
       return `<img alt="${alt}" src="${src}">`;
+    })
+    .replace(linkReg, (...args) => {
+      const groups = args.pop();
+      const linkText = groups.linkText;
+      const url = groups.url.trim();
+      return `<a href="${url}">${linkText}</a>`
     });
   
   rawHTML.innerText = htmlText;

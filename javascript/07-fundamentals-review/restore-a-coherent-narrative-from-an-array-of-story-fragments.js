30a8c944f6951196841508cc1dc9d919143b1e58
Le Dang Hoang Huy <ledanghoanghuy2004@gmail.com>
2026-08-29 17:42:54 +0700

    07-fundamentals review: restore-a-coherent-narrative-from-an-array-of-story-fragments (done)

--- javascript/07-fundamentals-review/restore-a-coherent-narrative-from-an-array-of-story-fragments.js
+++ javascript/07-fundamentals-review/restore-a-coherent-narrative-from-an-array-of-story-fragments.js
@@ -41,11 +41,109 @@ function compactFragments(fragments) {
 
 const compactedShuffledFragments = compactFragments(shuffledFragments);
 
-function sortFragments(fragments) {
+function merge(arr, left, mid, right) {
+  const n1 = mid - left + 1;
+  const n2 = right - mid;
+  const L = new Array(n1);
+  const R = new Array(n2);
+
+  for (let i = 0; i < n1; i++) {
+    L[i] = arr[i + left];
+  }
+  for (let j = 0; j < n2; j++) {
+    R[j] = arr[j + mid + 1];
+  }
+
+  let i = 0, 
+      j = 0, 
+      k = left;
   
+  while (i < n1 && j < n2) {
+    if (L[i]["id"] <= R[j]["id"]) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = R[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < n1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+
+  while (j < n2) {
+    arr[k] = R[j];
+    j++;
+    k++;
+  }
 }
 
-// test section
-const testFragments = [{ id: 1, text: "Hello" }, undefined];
+function mergeSort(arr, left, right) {
+  if (left >= right) {
+    return;
+  }
 
-console.log(compactFragments(testFragments));
+  const mid = Math.floor(left + (right - left) / 2);
+
+  mergeSort(arr, left, mid);
+  mergeSort(arr, mid + 1, right);
+  merge(arr, left, mid, right);
+}
+
+function sortFragments(shuffledFragments) {
+  const sortedFragments = [...shuffledFragments];
+
+  mergeSort(sortedFragments, 0, sortedFragments.length - 1);
+
+  return sortedFragments;
+}
+
+const sortedFragments = sortFragments(compactedShuffledFragments);
+
+function dedupeFragments(sortedFragments) {
+  const id = new Set();
+  const result = [];
+
+  for (const fragment of sortedFragments) {
+    if (id.has(fragment.id)) {
+      console.log("[DEDUPED]");
+    } else {
+      id.add(fragment.id);
+      result.push(fragment);
+    }
+  }
+
+  return result;
+}
+
+const dedupedFragments = dedupeFragments(sortedFragments);
+
+function fillMissingFragments(sortedFragments) {
+  const result = [...sortedFragments];
+
+  for (let i = 1; i < result.length; i++) {
+    if (result[i]["id"] !== result[i-1]["id"] + 1) {
+      result.splice(i, 0, {
+        id: result[i-1]["id"] + 1,
+        text: "[...]"
+      });
+      console.log("[FILLED]");
+    }
+  }
+
+  return result;
+}
+
+const filledFragments = fillMissingFragments(dedupedFragments);
+
+function assembleStory(sortedFragments) {
+  const fragTexts = sortedFragments.map(fragment => fragment.text);
+
+  return fragTexts.join("\n");
+}
+
+console.log(assembleStory(filledFragments));
